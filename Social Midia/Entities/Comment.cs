69cb17b03de14d313200dd79e331193ff20410d2
Ïsrael Alencar <israelalencarribeiro@gmail.com>
2021-11-12 10:51:13 -0400

    Social midia

---
+++ Social Midia/Entities/Comment.cs
@@ -0,0 +1,12 @@
+﻿namespace Social_Midia.Entities {
+    class Comment {
+        public string Text { get; set; }
+
+        public Comment() {
+        }
+
+        public Comment(string text) {
+            Text = text;
+        }
+    }
+}
